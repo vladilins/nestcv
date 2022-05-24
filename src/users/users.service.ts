f3e3f75ed85f97a5fc823813c902ddde1c2cd5f7
Vlad <ilin96inbox@gmail.com>
2022-05-24 21:57:06 +0900

    remove user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -30,5 +30,11 @@ export class UsersService {
     return this.repo.save(user);
   }
 
-  insert() {}
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return this.repo.remove(user);
+  }
 }
