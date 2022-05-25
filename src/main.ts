8be0e600373753d37418f05ab49d9566c96d4e6b
Vlad <ilin96inbox@gmail.com>
2022-05-25 21:16:06 +0900

    add cookie-session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['asdgaw'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
