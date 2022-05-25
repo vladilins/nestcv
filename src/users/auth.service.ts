eab1e28f5aa3a5d4c2120d7eaa6f93cf554d3441
Vlad <ilin96inbox@gmail.com>
2022-05-25 20:41:17 +0900

    signup

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // See if email already exists
+    const user = await this.usersService.find(email);
+    if (user.length) {
+      throw new BadRequestException('email in use');
+    }
+
+    // Hash the password
+
+    // Create a new user
+
+    // return the user
+  }
+
+  signin() {}
 }
