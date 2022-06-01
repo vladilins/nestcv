0e55f66609d80c5d9d017018424f699b19ab949c
Vlad <ilin96inbox@gmail.com>
2022-06-01 11:23:23 +0900

    password test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -25,4 +25,13 @@ describe('AuthService', () => {
   it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
   });
+
+  it('crates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('asdg@asgweg.com', 'asdf');
+
+    expect(user.password).not.toEqual('asdf');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
 });
